769dbd28eade895d7b45ded11f687139332eaa95
apci <apcitalia@hotmail.com>
2015-03-06 01:37:19 +0100

    show status list done
        <ListView
            android:layout_width="match_parent"
            android:layout_height="match_parent"
            android:id="@id/android:list" //ID SHOULD BE LIKE THIS
            android:layout_gravity="center_horizontal|top" >
        </ListView>

--- app/src/main/java/bluecup/com/personalnote/HomeActivity.java
+++ app/src/main/java/bluecup/com/personalnote/HomeActivity.java
@@ -1,36 +1,49 @@
 package bluecup.com.personalnote;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.List;
 
-public class HomeActivity extends ActionBarActivity {
+
+public class HomeActivity extends ListActivity {
+
+    protected List<ParseObject> mStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-
-
-
         ParseUser currentUser = ParseUser.getCurrentUser();
         if (currentUser != null) {
-            // do stuff with the user
+            // do some query then display them
+            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Status");
+            query.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> statusObjArray, ParseException e) {
+
+                    if(null == e){
+                        //return result
+                        mStatus = statusObjArray;
+
+                        StatusAdapter adapter = new StatusAdapter(getListView().getContext(), mStatus);
+                        setListAdapter(adapter);
+
+                    }else {
+                        // problem
+                    }
+                }
+            });
+
 
         } else {
             // show the signup or login screen
@@ -39,13 +52,6 @@ public class HomeActivity extends ActionBarActivity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -79,19 +85,5 @@ public class HomeActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-            return rootView;
-        }
-    }
 }
